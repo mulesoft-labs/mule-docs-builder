9fe665b4a1d48dc457bc5b4e2c9e4eef578b1c09
Sean Osterberg <sean.osterberg@gmail.com>
2015-08-15 21:54:26 -0700

    Update mule-docs-page-preview.rb
    Trying to fix brew error

--- mule-docs-page-preview.rb
+++ mule-docs-page-preview.rb
@@ -9,7 +9,7 @@ class MuleDocsPagePreview < Formula
   depends_on :java
 
   def install
-    system "mvn clean package -DskipTests"
+    system "mvn clean install -DskipTests"
     
     libexec.install "mule-docs-single-page-builder/target/mule-docs-single-page-builder-1.0.0-SNAPSHOT.jar"
     
